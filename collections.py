8f4cf0006c81bcde5296d767012f0434576deb40
Michael <71185790+Michaelredaa@users.noreply.github.com>
2023-05-03 17:33:40 +0300

    Create collections.py

---
+++ collections.py
@@ -0,0 +1,11 @@
+def get_all_collections(prim: Usd.Prim) -> dict:
+    data = {}
+    for col in Usd.CollectionAPI.GetAllCollections(prim):
+        data[col.GetName()] = [str(p.GetPrimPath()) for p in col.GetIncludesRel().GetTargets()]
+
+    return data
+
+
+def get_collection_by_name(prim: Usd.Prim, col_name: str) -> Usd.CollectionAPI:
+    collection_prim = Usd.CollectionAPI.GetCollection(prim, col_name)
+    return collection_prim
