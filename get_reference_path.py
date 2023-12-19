dfe931ed5dbe52b6a73231c7f3e3634259474e88
Michael Reda <eng.michaelreda@gmail.com>
2023-12-19 23:50:56 +1100

    Add get_references_paths

--- get_reference_path.py
+++
@@ -1,8 +0,0 @@
-
-
-def get_reference_path(prim):
-    return prim.GetMetadata('references').GetAddedOrExplicitItems()[0].assetPath
-
-
-def get_payload_path(prim):
-    return prim.GetMetadata('payload').GetAddedOrExplicitItems()[0].assetPath
