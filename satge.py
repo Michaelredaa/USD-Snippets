79c34c926e2a2ea2f79281842d29ba7e10b30fbc
Michael Reda <eng.michaelreda@gmail.com>
2024-01-03 21:57:17 +1100

    add copy_prim

--- satge.py
+++ satge.py
@@ -60,3 +60,23 @@ def create_stage_with_sublayer(stage_filepath, sublayer_paths):
             with open_layer(sublayer_path) as sublayer:
                 if sublayer.identifier not in root_layer.subLayerPaths:
                     root_layer.subLayerPaths.append(sublayer.identifier)
+
+
+def copy_prim(source_stage: Usd.Stage, source_path: Sdf.Path,
+              dist_stage: Usd.Stage, dist_path: Sdf.Path, ):
+
+    sprim = source_stage.GetPrimAtPath(source_path)
+    dprim = dist_stage.DefinePrim(dist_path, sprim.GetTypeName())
+
+    for sprop in sprim.GetProperties():
+        if isinstance(sprop, Usd.Relationship):
+            targets = sprop.GetTargets()
+            if targets:
+                dprop = dprim.CreateRelationship(sprop.GetName())
+                dprop.SetTargets(targets)
+
+        elif isinstance(sprop, Usd.Attribute):
+            value = sprop.Get()
+            if value:
+                dattr = dprim.CreateAttribute(sprop.GetName(), sprop.GetTypeName())
+                dattr.Set(value)
