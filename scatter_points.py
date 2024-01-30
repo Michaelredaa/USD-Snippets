38578fc2a4a05a1f233183519b26d08afd00bbbf
Michael Reda <71185790+Michaelredaa@users.noreply.github.com>
2024-01-30 15:19:49 +1100

    Add scatter_points_with_dis

--- scatter_points.py
+++ scatter_points.py
@@ -59,6 +59,48 @@ def scatter_points(prim, num_points, seed=101):
 
     return np.array(new_points)
 
+def scatter_points_with_dis(prim, num_points, min_distance= 0.1, seed=101):
+
+    mesh = UsdGeom.Mesh(prim)
+
+    points_attr = mesh.GetPointsAttr()
+    fvCount_attr = mesh.GetFaceVertexCountsAttr()
+    fvIndices_attr = mesh.GetFaceVertexIndicesAttr()
+
+    points_values = np.array(points_attr.Get())
+    fvIndices_values = np.array(fvIndices_attr.Get())
+
+    faces_values = fvIndices_values.reshape(-1, 4)
+
+    random_values_list = random_groups_vectors(num_points, faces_values.shape[0], seed=seed)
+
+    new_points = np.empty((0, 3), dtype=np.float32)
+    for i, face in enumerate(faces_values):
+
+        p0 = points_values[face[0]]
+        p1 = points_values[face[1]]
+        p2 = points_values[face[2]]
+        p3 = points_values[face[3]]
+
+        v1 = p1 - p0
+        v2 = p3 - p0
+
+        
+        # newp = p0 + (r1 * v1) + (r2 * v2)
+        
+        # for r1, r2 in random_values_list[i]:
+        #     # barycentric coordinates
+        #     newp = p0 + (r1 * v1) + (r2 * v2)
+            
+        #     if all(np.linalg.norm(newp-existing_point) >= min_distance for existing_point in new_points):
+        #         new_points.append(newp)
+        
+        
+        candidate_points = p0 + (random_values_list[i][:, 0, np.newaxis] * v1) + (random_values_list[i][:, 1, np.newaxis] * v2)
+        # Check the distance to existing points
+        distances = np.linalg.norm(candidate_points[:, np.newaxis] - new_points, axis=2)
+        if np.all(distances >= min_distance):
+            new_points = np.vstack((new_points, candidate_points))
 
 particle_set = UsdGeom.Points.Define(stage, '/grid1/points')
 particle_set.CreatePointsAttr(scatter_points(prim, scatter))
