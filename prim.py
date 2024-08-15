fef74b04901a7e50bb148b13a7c6d018830cc3c1
Michael Reda <71185790+Michaelredaa@users.noreply.github.com>
2024-08-15 14:28:04 +1000

    Create a box from bounds

--- prim.py
+++ prim.py
@@ -62,3 +62,45 @@ def get_bbox_with_xform(prim: Usd.Prim):
     bmax = Gf.Vec3d(bmax[0]*sx, bmax[1]*sy, bmax[2]*sz)
 
     return Gf.BBox3d(Gf.Range3d(bmin, bmax)).GetBox()
+
+
+
+def create_bbox_prim(prim: Usd.Prim, box_path: str) -> UsdGeom.Mesh:
+    """Create a box the represent the bbox for prim"""
+
+    stage = prim.GetStage()
+    bbox = get_bbox_with_xform(prim)
+
+    """
+    https://openusd.org/release/api/class_gf_range3d.html#af3e88ddd9c61229ce81086a4bdac1bc8
+    Returns the ith corner of the range, in the following order: LDB, RDB, LUB, RUB, LDF, RDF, LUF, RUF.
+    Where L/R is left/right, D/U is down/up, and B/F is back/front.
+    # 0 1 2 3 5 4 6 7
+    """
+
+    face_vertex_indices = [
+        [0, 1, 3, 2],
+        [5, 4, 6, 7],
+        [7, 6, 2, 3],
+        [4, 5, 1, 0],
+        [4, 0, 2, 6],
+        [1, 5, 7, 3]
+    ]
+
+    face_vertex_indices_flat = [idx for face in face_vertex_indices for idx in face]
+    face_vertex_counts = [4] * len(face_vertex_indices)
+
+    points = []
+    for i in range(8):
+        corner = bbox.GetBox().GetCorner(i)
+        points.append(corner)
+
+    mesh = UsdGeom.Mesh.Define(stage, box_path)
+
+    mesh.CreateFaceVertexCountsAttr(face_vertex_counts)
+    mesh.CreateFaceVertexIndicesAttr(face_vertex_indices_flat)
+    mesh.CreatePointsAttr(points)
+
+    mesh.GetSubdivisionSchemeAttr().Set("none")
+
+    return mesh
