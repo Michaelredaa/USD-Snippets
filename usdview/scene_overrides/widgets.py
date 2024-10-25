8f2aab222023501aa20a53d94ed687b7ce2dc30f
Michael Reda <71185790+Michaelredaa@users.noreply.github.com>
2024-10-25 17:55:50 +1100

    Update compose and decompose

--- usdview/scene_overrides/widgets.py
+++ usdview/scene_overrides/widgets.py
@@ -75,21 +75,34 @@ def is_iterable(obj):
 def decompose_matrix(matrix):
     transform = Gf.Transform(matrix)
     translation = transform.GetTranslation()
-    rotation = transform.GetRotation().Decompose(Gf.Vec3d(1, 0, 0), Gf.Vec3d(0, 1, 0),
-                                                 Gf.Vec3d(0, 0, 1))  # Decomposes into Euler XYZ
+    
+    rotation_quat = transform.GetRotation().GetQuaternion()
+    quatd = Gf.Quatd(rotation_quat.GetReal(), rotation_quat.GetImaginary())  # Create Gf.Quatd from real and imaginary parts
+    
+    rotation = Gf.Rotation()
+    rotation.SetQuat(quatd)
+    
+    # Decompose rotation into Euler angles (YXZ order to avoid gimbal lock on Y rotations)
+    euler_rotation = rotation.Decompose(Gf.Vec3d(1, 0, 0), Gf.Vec3d(0, 1, 0), Gf.Vec3d(0, 0, 1))
     scale = transform.GetScale()
-    return translation, rotation, scale
+    
+    return translation, euler_rotation, scale
 
 
 def compose_matrix(translation, rotation, scale):
     transform = Gf.Transform()
+    
     transform.SetScale(Gf.Vec3d(*scale))
-
-    rot = Gf.Rotation(Gf.Vec3d(1, 0, 0), rotation[0]) * \
-          Gf.Rotation(Gf.Vec3d(0, 1, 0), rotation[1]) * \
-          Gf.Rotation(Gf.Vec3d(0, 0, 1), rotation[2])
-    transform.SetRotation(rot)
+    
+    # Apply rotation using Euler angles in YXZ order
+    rotX = Gf.Rotation(Gf.Vec3d(1, 0, 0), rotation[0])
+    rotY = Gf.Rotation(Gf.Vec3d(0, 1, 0), rotation[1])
+    rotZ = Gf.Rotation(Gf.Vec3d(0, 0, 1), rotation[2])
+    combined_rotation = rotX * rotY * rotZ
+    transform.SetRotation(combined_rotation)
+   
     transform.SetTranslation(Gf.Vec3d(*translation))
+    
     return transform.GetMatrix()
 
 
